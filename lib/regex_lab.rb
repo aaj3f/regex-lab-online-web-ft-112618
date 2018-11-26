960061e74134dfe34d736d11680937c585576777
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-26 18:44:59 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\b[A-Z]\w*[W]\b/)
+  if text.match(/\b[A-Z]\w*[W]\b/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
